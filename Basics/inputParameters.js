1baa5beab594d75507908a278972317b8f150862
Andrea <abisegna1985@gmail.com>
2023-08-24 20:06:16 +0200

    First Commit

---
+++ Basics/inputParameters.js
@@ -0,0 +1,27 @@
+// Here we see how to handle simple functions with parameters. 
+//How to log a result from a fucntion
+// How to transform a function in arrow function
+
+
+let func = (a,b) => a + b;  // arrow function
+
+console.log(func(3, 8));
+
+/*function sottr(z,t){
+    return z - t;
+}*/
+
+let x=sottr(3,8);
+console.log(x);
+
+let sottr = (z,t) => z - t;
+
+
+
+
+
+//let x = func(5, 7);
+//Hoisting
+
+//console.log(func(4,6));//
+//console.log(x);
