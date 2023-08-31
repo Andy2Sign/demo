bb9be02dc8100d8632578f429d49104f5497616d
Andrea <abisegna1985@gmail.com>
2023-08-31 08:04:02 +0200

    new directory

---
+++ Basics/Exercise/funcExercise.js
@@ -0,0 +1,30 @@
+/*
+1st exercise
+Create a function checkType() which accept an input as parameter
+
+The input will be a number
+
+First we need to check if the "typeOf" the parameter is a number
+
+If is a number return the number
+
+This number will be used in another function which follows the checkType()
+
+This function is called checkPariDispari()
+
+Accept a parameter which will be the the number returned from the previous function
+
+check if the number is Pari o Dispari
+
+console.log the result of the function
+*/
+
+
+
+/*
+2nd exercise
+
+Create a function that accept a string a input parameter which return an anonymous function which also accept a string as input parameter
+
+At the end we need to be able to display in the console "Hello World";
+ */
