216891ddf0f9aaeb7fc8b2126b20196e44b2edb5
Andrea <abisegna1985@gmail.com>
2023-09-03 15:03:08 +0200

    hoist & callback

--- Basics/Exercise/funcExercise.js
+++ Basics/Exercise/funcExercise.js
@@ -27,4 +27,61 @@ console.log the result of the function
 Create a function that accept a string a input parameter which return an anonymous function which also accept a string as input parameter
 
 At the end we need to be able to display in the console "Hello World";
+function a(x) {
+    return function(y) {
+        console.log(x + y)
+    }
+}
+
+let z = a("Hello ");
+z("World");
  */
+
+
+
+// cosa viene loggato prima
+
+// const greetIta = () => {
+// 	console.log("Ciao");
+// }
+
+
+
+
+// const toUpper = (string) => string.toUpperCase();
+// console.log(toUpper("ciao"));
+
+
+// const greetIta = () => "Ciao";
+// let x = greetIta();
+
+// let a = "Andrea " + x;
+// console.log(a);
+
+
+// const greetEng = () => {
+//     console.log("Hi");
+// }
+
+// greetEng();
+hoist();
+
+console.log(a);
+
+function hoist() {
+    a = 20;
+    var b = 100;
+}
+
+
+/* 
+Accessible as a global variable outside hoist() function
+Output: 20
+*/
+
+//console.log(b);
+/*
+Since it was declared, it is confined to the hoist() function scope.
+We can't print it out outside the confines of the hoist() function.
+Output: ReferenceError: b is not defined
+*/
---
+++ Basics/callBack.js
@@ -0,0 +1,69 @@
+//this is a function which accept an input parameter that will be returned toUpperCase 
+
+// function toUpper(x) {
+//     return x.toUpperCase();
+// }
+
+//this is another function which accpet two input parameters: y, formatter
+//this will logs the first input passed as parameter into the second input
+// it means the second inout is a function
+//A function can accept as parameter another function previously declared
+//this is a callBack
+//a callback is a function which has the possibility to be passed as 
+//parameter and manipulate others parameters passed with itself
+
+// const giveName = (y, formatter) => {
+//     console.log(formatter(y));
+// }
+
+//here we pass the parameters
+
+// let res = giveName("andrea", toUpper);
+
+// console.log(res)
+
+
+
+// const callback = () => {
+//     console.log("Coming from callback");
+// }
+
+// const higherOrder = (func) => {
+//     console.log("About to call callback");
+
+//     func() //Callback function is invoked
+
+//     console.log("Callback has been invoked")
+// }
+
+// higherOrder(callback);
+
+// result is:
+
+// "About to call callback"
+// "Coming from callback"
+// "Callback has been invoked"
+
+
+//We have to build a function which return a random number already parsed
+// we need to print this number in the console only if it is Odd trhough an higerOrder function
+
+
+const generate = () => {
+    return Math.ceil(Math.random(100) * 1000);
+}
+let number = generate();
+console.log(number);
+
+//here below the callback we'll use as parameter
+
+const checkNumber = (num) => num % 2 != 0 ? "the number is: " + num : num + " is not Odd";
+
+//this below the HigherOrder function which accept the callback as parameter
+
+const printRes = (value, check) => {
+
+    console.log(check(value));
+}
+
+console.log(printRes(number, checkNumber));
