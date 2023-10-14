1b030589f04a77abbb120d0456d2a2d0d3bcba1e
Andrea <abisegna1985@gmail.com>
2023-10-14 21:37:05 +0200

    exercise array not yet done

--- Basics/Exercise/arrayExerciseWithCss/index.js
+++ Basics/Exercise/arrayExerciseWithCss/index.js
@@ -2,26 +2,24 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const checkPariDispari = (num) => {
     if (num % 2 == 0) {
-        //console.log(`${num} è Pari`);
-        return `${num} è Pari`;
+        console.log(`${num} è Pari`);
+        //return `${num} è Pari`;
         //document.getElementById("myId").innerHTML = `${num} è Pari`;
     } else {
-        //console.log(`${num} è Dispari`)
-        return `${num} è Dispari`;
-        //document.getElementById("myId").innerHTML = `${num} è Dispari`;
+        console.log(`${num} è Dispari`)
+            //return `${num} è Dispari`;
+            //document.getElementById("myId").innerHTML = `${num} è Dispari`;
     }
 };
 
 const display = () => {
-    // let filtered = numbers.filter(checkPariDispari)
-    // filtered.map((el) => {
+    //numbers.filter(checkPariDispari)
 
-    //     document.getElementById('myDiv').innerHTML = el;
-    // });
-
-    document.getElementById('myId').innerHTML = numbers.map((el) => {
-        el + " helo";
-    })
+    numbers.map((el) => `<li>${checkPariDispari(el)}</li>`)
 
 
+
+    // document.getElementById('myId').innerHTML = numbers.forEach((el) => {
+    //     el + " helo";
+    // })
 }
