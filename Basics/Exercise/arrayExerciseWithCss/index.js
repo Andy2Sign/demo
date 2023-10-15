34efc8e87921e8a053b82c3fb93391efbf44b42a
Andrea <abisegna1985@gmail.com>
2023-10-15 19:46:55 +0200

    cleanup

--- Basics/Exercise/arrayExerciseWithCss/index.js
+++ Basics/Exercise/arrayExerciseWithCss/index.js
@@ -1,16 +1,16 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const checkPariDispari = (num) => {
-    if (num % 2 == 0) {
-        //console.log(`${num} è Pari`);
-        return `${num} è Pari`;
-        //document.getElementById("myId").innerHTML = `${num} è Pari`;
-    } else {
-        //console.log(`${num} è Dispari`)
-        return `${num} è Dispari`;
-        //document.getElementById("myId").innerHTML = `${num} è Dispari`;
-    }
-};
+// const checkPariDispari = (num) => {
+//     if (num % 2 == 0) {
+//         //console.log(`${num} è Pari`);
+//         return `${num} è Pari`;
+//         //document.getElementById("myId").innerHTML = `${num} è Pari`;
+//     } else {
+//         //console.log(`${num} è Dispari`)
+//         return `${num} è Dispari`;
+//         //document.getElementById("myId").innerHTML = `${num} è Dispari`;
+//     }
+// };
 
 let btn = document.getElementById("btn");
 
@@ -28,7 +28,6 @@ btn.addEventListener("click", () => {
         //elementList.textContent = filtered;
 
         //elementList == "è Pari" ? elementList.classList.add("pari") : elementList.classList.add("dispari");
-
 
         divContainer.appendChild(elementList);
     });
