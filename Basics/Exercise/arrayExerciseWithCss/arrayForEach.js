c26c83d006117903261a70e920daa2fd575baba1
Andrea <abisegna1985@gmail.com>
2023-10-27 12:44:23 +0200

    last commit

---
+++ Basics/Exercise/arrayExerciseWithCss/arrayForEach.js
@@ -0,0 +1,20 @@
+let arr = ['gennaio', 'febbraio', ' marzo ', 'aprile '];
+
+//console.log(arr[2].trim());
+
+
+
+
+const x = (z) => {
+    if (z.charAt(0) == "g") {
+        return z
+    } else {
+        "sorry"
+    }
+}
+
+
+
+let y = arr.filter(x);
+
+console.log(y);
