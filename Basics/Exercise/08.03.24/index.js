c6fc80ab928025e34453715cc3affbd7a9ea5c38
Andrea <abisegna1985@gmail.com>
2024-03-08 20:10:03 +0100

    new exercise

---
+++ Basics/Exercise/08.03.24/index.js
@@ -0,0 +1,39 @@
+// swap array's index position
+
+//let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const generate = () => {
+//     for (let i = 0; i < 10; i++) {
+//         randomList.push(Math.ceil(Math.random() * 1000));
+//     }
+//     return randomList
+// };
+
+// let numbersList = generate();
+// console.log(numbersList);
+
+// let mapping = [
+//     [0, 5],
+//     [1, 4],
+//     [2, 3],
+//     [3, 2],
+//     [4, 1],
+//     [5, 0]
+// ];
+
+// let elements = [1, 2, 3, 4, 5, 6];
+
+// function swapElements(elements, mapping) {
+
+//     let tmp = new Array(elements.length);
+//     for (let i = 0, l = mapping.length; i < l; i++) {
+//         tmp[mapping[i][1]] = elements[mapping[i][0]];
+//     }
+//     for (let i = 0, l = elements.length; i < l; i++) {
+//         elements[i] = tmp[i];
+//     }
+// }
+
+// swapElements(elements, mapping);
+
+// console.log(elements);​
